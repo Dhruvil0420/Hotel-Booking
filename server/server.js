6374a9b21f1a8917ab8a56355f4669ac80a1c103
Dhruvil0420 <dhruvilparmar1819@gmail.in>
2026-02-28 20:40:51 +0530

    Remove error

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(clerkMiddleware());
 
 // API to listen to Clerk Webhooks
-app.use(' ',clerkWebhooks)  
+app.use('/api/clerk',clerkWebhooks)  
 app.get('/',(req,res) => {
     res.send("API is working");
 })
